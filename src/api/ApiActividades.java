ba0ca074337061d08a50fcf12ecc1ffde162942c
Igor <Igor@IgorPc>
2020-04-20 10:53:08 +0200

    first commit

---
+++ src/api/ApiActividades.java
@@ -0,0 +1,64 @@
+package api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONStringer;
+
+import jdk.nashorn.internal.ir.debug.JSONWriter;
+import jdk.nashorn.internal.parser.JSONParser;
+import modelo.bean.Actividad;
+import modelo.dao.ModeloActividad;
+
+/**
+ * Servlet implementation class ApiActividades
+ */
+@WebServlet("/ApiActividades")
+public class ApiActividades extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ApiActividades() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ModeloActividad mActividades = new ModeloActividad();
+		ArrayList<Actividad> actividades = mActividades.selectAll();
+		
+		response.setHeader("Access-Control-Allow-Origin", "*"); // jsonp deia denean ez da behar
+		response.setContentType("application/json");	//erantzunaren MIME mota zein den zehazten du
+		//response.setCharacterEncoding("UTF-8");			//kodifikazioa zehazten du
+		
+		String jsonString = JSONStringer.valueToString(actividades);
+		
+//	    PrintWriter out = new PrintWriter(new OutputStreamWriter(response.getOutputStream(), "UTF8"), true);
+		PrintWriter out = response.getWriter();
+		out.print(jsonString);
+		out.flush();
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/api/ApiUsuarios.java
@@ -0,0 +1,65 @@
+package api;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import modelo.bean.Actividad;
+import modelo.bean.Usuario;
+import modelo.dao.ModeloActividad;
+import modelo.dao.ModeloUsuario;
+
+/**
+ * Servlet implementation class ApiUsuarios
+ */
+@WebServlet("/ApiUsuarios")
+public class ApiUsuarios extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ApiUsuarios() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ModeloUsuario mUsuarios = new ModeloUsuario();
+		ArrayList<Usuario> usuarios = mUsuarios.selectAll();
+		
+		
+		response.setHeader("Access-Control-Allow-Origin", "*"); // jsonp deia denean ez da behar
+		response.setContentType("application/json");	//erantzunaren MIME mota zein den zehazten du
+		//response.setCharacterEncoding("UTF-8");			//kodifikazioa zehazten du
+		
+		String jsonString = JSONStringer.valueToString(usuarios);
+		
+//	    PrintWriter out = new PrintWriter(new OutputStreamWriter(response.getOutputStream(), "UTF8"), true);
+		PrintWriter out = response.getWriter();
+		out.print(jsonString);
+		out.flush();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
