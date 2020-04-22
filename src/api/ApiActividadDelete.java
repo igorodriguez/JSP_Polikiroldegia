3a1a87cfc3ebb73b33d5c260e4637e77ffba1fdd
Igor <Igor@IgorPc>
2020-04-22 17:59:38 +0200

    ApiActividadDelete eginda

---
+++ src/api/ApiActividadDelete.java
@@ -0,0 +1,58 @@
+package api;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.dao.ModeloActividad;
+
+/**
+ * Servlet implementation class ApiActividadDelete
+ */
+@WebServlet("/ApiActividadDelete")
+public class ApiActividadDelete extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ApiActividadDelete() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		try {
+			ModeloActividad mActividad = new ModeloActividad();
+			
+			mActividad.delete(id);
+			mActividad.getConexion().close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
