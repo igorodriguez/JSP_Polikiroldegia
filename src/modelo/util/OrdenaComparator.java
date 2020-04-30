e8c269620f095073b52f5f21b8b4cfe37b67f7ee
Igor <Igor@IgorPc>
2020-04-30 10:37:55 +0200

    OrdenaComparator amaitu gabe

---
+++ src/modelo/util/OrdenaComparator.java
@@ -0,0 +1,15 @@
+package modelo.util;
+
+import java.util.Comparator;
+
+import modelo.bean.Usuario;
+
+public class OrdenaComparator implements Comparator <Usuario>{
+
+	@Override
+	public int compare(Usuario o1, Usuario o2) {
+		
+		return 0;
+	}
+
+}
